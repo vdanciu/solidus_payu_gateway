488cedc89645a20d7757e1458c142ef110c53baa
Vlad Danciu <vdanciu@MacBook-Pro.local>
2018-06-15 13:24:13 +0300

    keep on trying notify

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Spree::Core::Engine.routes.draw do
   get "/payu/gateway", to: "payu#gateway", as: :payu_gateway
-  put "/payu/notify", to: "payu#notify", as: :payu_notify
+  post "/payu/notify", to: "payu#notify", as: :payu_notify
 end
