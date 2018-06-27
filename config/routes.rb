5412e38ac44b475b0c5f495a9667c9a132261158
Vlad Danciu <vdanciu@MacBook-Pro.local>
2018-06-27 22:42:21 +0300

    serve notify for post and get

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.routes.draw do
   get "/payu/gateway", to: "payu#gateway", as: :payu_gateway
   post "/payu/notify", to: "payu#notify", as: :payu_notify
-  get "/payu/notify", to: "payu#notify", as: :payu_notify
+  get "/payu/notify", to: "payu#notify"
   get "/payu/continue/:id", to: "payu#continue", as: :payu_continue
 end
