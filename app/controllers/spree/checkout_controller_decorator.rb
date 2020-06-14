8b6230d52f35c0fc1ca86263809197daa326a40d
Vlad <vdanciu@yahoo.com>
2020-06-14 12:58:05 +0300

    refactor the way redirect is done

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -22,6 +22,28 @@ module SolidusPayuGateway
       end
     end
 
+    def transition_forward
+      if @order.can_complete?
+        @order.complete unless payment_redirect_required?
+        false
+      else
+        @order.next
+      end
+    end
+
+
+    def send_to_next_state
+      if @order.state == "confirm"
+        if payment_redirect_required?
+          redirect_to get_order_payment.redirect_url
+          true
+        end
+      else
+        super
+      end
+    end
+
+
     private
 
     def follow_payment_redirect
@@ -33,6 +55,16 @@ module SolidusPayuGateway
         true
       end
     end
+
+
+    def payment_redirect_required?
+      get_order_payment.tap { |payment| return payment.try(:redirect_url)}
+    end
+
+    def get_order_payment
+      @order.payments.valid.last
+    end
+
   end
 end
 
