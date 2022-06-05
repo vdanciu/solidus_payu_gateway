94c7f29a09d04770d8a8cfbf979fb63e5b5b5dd5
Vlad <vdanciu@yahoo.com>
2022-06-05 10:32:29 +0300

    Use 'orderData' to access order number

--- app/controllers/spree/payu_controller.rb
+++ app/controllers/spree/payu_controller.rb
@@ -39,7 +39,7 @@ module Spree
     end
 
     def notify
-      order_id = params['REFNOEXT'] || params['merchantPaymentReference']
+      order_id = params['REFNOEXT'] || (params['orderData'] || {})['merchantPaymentReference']
       status = params['ORDERSTATUS']
       log_info(order_id, "notify params: #{params}")
       log_info(order_id, "notify, ORDERSTATUS: #{status}")
